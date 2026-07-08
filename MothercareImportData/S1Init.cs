2d341a849586a5663b913e0cb338bb2bf7863f93
emoustroufas <emoustroufas@logistic-i.gr>
2026-07-08 12:10:10 +0300

    add code

---
+++ MothercareImportData/S1Init.cs
@@ -0,0 +1,17 @@
+﻿using Softone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MothercareImportData
+{
+    public class S1Init : TXCode
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
